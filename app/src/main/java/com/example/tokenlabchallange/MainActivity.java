fde1fb273fa10f3033b42555779570afd18d37e3
Lui <luifr@usp.br>
2019-02-10 14:49:05 -0200

    Implemented sort menu

--- app/src/main/java/com/example/tokenlabchallange/MainActivity.java
+++ app/src/main/java/com/example/tokenlabchallange/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.tokenlabchallange;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -20,6 +25,7 @@ import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.Collection;
@@ -27,6 +33,9 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
+
+    MovieAdapter mAdapter;
+    Movie[] movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         Gson gson = new Gson();
         Type collectionType = new TypeToken<Movie[]>() {
         }.getType();
-        Movie[] movies = gson.fromJson(jsonMovies, collectionType);
+        movies = gson.fromJson(jsonMovies, collectionType);
         RecyclerView listViewer = findViewById(R.id.movieListContainer);
         listViewer.setHasFixedSize(true);
         RecyclerView.LayoutManager lm = new LinearLayoutManager(this);
@@ -78,11 +87,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        MovieAdapter mAdapter = new MovieAdapter(this, movies);
+        mAdapter = new MovieAdapter(this, movies);
         listViewer.setAdapter(mAdapter);
 
         listViewer.addItemDecoration(new DividerItemDecoration(listViewer.getContext(),((LinearLayoutManager) lm).getOrientation()));
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.alphabetic){
+            Arrays.sort(movies, new Comparator<Movie>() {
+                @Override
+                public int compare(Movie lhs, Movie rhs) {
+                    return lhs.title.compareTo(rhs.title);
+                }
+            });
+        }
+        else if(id == R.id.alphabeticR){
+            Arrays.sort(movies, new Comparator<Movie>() {
+                @Override
+                public int compare(Movie lhs, Movie rhs) {
+                    return rhs.title.compareTo(lhs.title);
+                }
+            });
+        }
+        else if(id == R.id.year){
+            Arrays.sort(movies, new Comparator<Movie>() {
+                @Override
+                public int compare(Movie lhs, Movie rhs) {
+                    return lhs.release_date.split("-")[0].compareTo(rhs.release_date.split("-")[0]);
+                }
+            });
+        }
+        else if(id == R.id.yearR){
+            Arrays.sort(movies, new Comparator<Movie>() {
+                @Override
+                public int compare(Movie lhs, Movie rhs) {
+                    return rhs.release_date.split("-")[0].compareTo(lhs.release_date.split("-")[0]);
+                }
+            });
+        }
+
+        mAdapter.notifyDataSetChanged();
+        return super.onOptionsItemSelected(item);
+    }
 }
